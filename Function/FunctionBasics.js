9e475c33bf262cd79d3e2aab4f781e65cb04bc46
Bhavesh Singh <bhaveshhsinghh30@gmail.com>
2024-02-05 05:04:09 +0000

    Function

---
+++ Function/FunctionBasics.js
@@ -0,0 +1,9 @@
+// function saymyname(){
+//     console.log("Hello")
+// }
+// saymyname()
+
+function sum(number1, number2) {
+    console.log('Sum of numbers ' + number1 + ' and ' + number2 + ' is ' + (number1 + number2));
+}
+sum(10, 2);
