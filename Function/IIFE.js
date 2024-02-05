537f13c6db49b9e97829c33e1107f01fa3554421
Bhavesh Singh <bhaveshhsinghh30@gmail.com>
2024-02-05 14:41:54 +0000

    IIFE

---
+++ Function/IIFE.js
@@ -0,0 +1,17 @@
+//IIFE formate --> ()()
+
+// function chai(){
+//     console.log("Have Tea")
+// }
+// chai()
+
+//IIFA
+// (function chai(){
+//     console.log("Have Tea")
+// })()
+
+
+//arrowFunction
+
+( (name) => { console.log("Have tea"); } ) ()
+
